3ca63e2cac6c4e4be55adbf484b1861c6fc367dc
SeongJung0106 <fjunkimsj@naver.com>
2016-06-04 15:27:38 +0900

    Fix AbstractCollection class

--- src/com/nts/cleancode/collections/AbstractCollection.java
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -1,14 +1,14 @@
 package com.nts.cleancode.collections;
 
 public abstract class AbstractCollection {
-	
+
 	protected static int INITIAL_CAPACITY = 10;
 	protected Object[] elements = new Object[INITIAL_CAPACITY];
 	protected int size = 0;
 	protected boolean readOnly;
-	
+
 	public void addAll(AbstractCollection collection) {
-		for (int i=0; i < collection.size(); i++) {
+		for (int i = 0; i < collection.size(); i++) {
 			if (!contains(collection.get(i)))
 				add(collection.get(i));
 		}
@@ -16,46 +16,55 @@ public abstract class AbstractCollection {
 
 	public void add(Object key, Object value) {
 	}
+
 	public boolean isEmpty() {
 		return size == 0;
 	}
+
 	public boolean contains(Object element) {
 		for (int i = 0; i < size; i++)
 			if (elements[i].equals(element))
 				return true;
 		return false;
 	}
+
 	public int size() {
 		return size;
 	}
+
 	public int capacity() {
 		return elements.length;
 	}
+
 	public void setReadOnly(boolean b) {
 		readOnly = b;
 	}
+
 	public Object get(int index) {
 		return elements[index];
 	}
+
 	public void add(Object element) {
 		if (readOnly)
 			return;
-		
-		if (shouldGrow()) 
+
+		if (shouldGrow())
 			grow();
-		
+
 		addElement(element);
 	}
+
 	private void addElement(Object element) {
 		elements[size++] = element;
 	}
+
 	private void grow() {
-		Object[] newElements =
-			new Object[elements.length + 10];
+		Object[] newElements = new Object[elements.length + 10];
 		for (int i = 0; i < size; i++)
 			newElements[i] = elements[i];
 		elements = newElements;
 	}
+
 	private boolean shouldGrow() {
 		return size + 1 > elements.length;
 	}
@@ -63,13 +72,13 @@ public abstract class AbstractCollection {
 	public boolean remove(Object element) {
 		if (readOnly)
 			return false;
-		
+
 		for (int i = 0; i < size; i++)
 			if (elements[i].equals(element)) {
 				removeElementAt(i);
 				return true;
 			}
-		
+
 		return false;
 	}
 
